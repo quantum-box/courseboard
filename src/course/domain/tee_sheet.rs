ebf565ce13f45a9a84114956d6ce051bb1c6eb1c
Takanori Fukuyama <takanori.fukuyama@gmail.com>
2026-07-19 18:27:19 +0900

    Format the clippy fix

--- src/course/domain/tee_sheet.rs
+++ src/course/domain/tee_sheet.rs
@@ -214,8 +214,7 @@ pub fn format_datetime_with_offset(value: DateTime<chrono::Utc>, offset: FixedOf
 }
 
 pub fn format_jst_wall_clock(date: NaiveDate, hour: u32, minute: u32, jst: FixedOffset) -> String {
-    let naive =
-        date.and_time(NaiveTime::from_hms_opt(hour, minute, 0).unwrap_or(NaiveTime::MIN));
+    let naive = date.and_time(NaiveTime::from_hms_opt(hour, minute, 0).unwrap_or(NaiveTime::MIN));
     jst.from_local_datetime(&naive)
         .single()
         .unwrap_or_else(|| jst.from_utc_datetime(&naive))
